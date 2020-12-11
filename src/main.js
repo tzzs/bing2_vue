d1b24998b895b8f9d039a6e355986f466694de51
TZZ <1789803837@qq.com>
2020-12-11 23:00:58 +0800

    完成hover处理

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-// import '../public/css/index.css'
+import '../public/css/index.css'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,17 +16,110 @@
     </v-app-bar>
 
     <v-main class="main">
-      <v-container>
+      <v-container class="">
         <v-row>
           <v-col
-              v-for="n in 9"
+              v-for="n in 3"
               :key="n"
               cols="4"
           >
-            <v-card height="200"></v-card>
+            <v-hover v-slot:default="{ hover }">
+              <v-card
+
+                  height="200"
+                  :elevation="hover ? 4 : 0"
+              ></v-card>
+            </v-hover>
           </v-col>
         </v-row>
+        <v-divider></v-divider>
+        <div class="library-headline"></div>
         <v-row>
+          <v-col cols="4">
+            <v-card
+                class="mx-auto v-card"
+                hover
+                width=""
+            >
+              <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                  height="200px"
+              ></v-img>
+
+              <v-card-title>
+                Exploring Color on Google Maps
+              </v-card-title>
+
+              <v-card-subtitle>
+                1,000 miles of wonder
+              </v-card-subtitle>
+
+              <v-card-actions>
+                <div>
+                  Editorial
+                </div>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="4">
+            <v-hover v-slot:default="{ hover }">
+              <v-card
+                  class="mx-auto"
+                  width=""
+                  max-width="344"
+                  :elevation="hover ? 4 : 0"
+              >
+                <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                    height="200px"
+                ></v-img>
+
+                <v-card-title>
+                  Top western road trips
+                </v-card-title>
+
+                <v-card-subtitle>
+                  1,000 miles of wonder
+                </v-card-subtitle>
+
+                <v-card-actions>
+                  <v-btn
+                      color="orange lighten-2"
+                      text
+                  >
+                    Explore
+                  </v-btn>
+
+                  <v-spacer></v-spacer>
+
+                  <v-btn
+                      icon
+                      @click="show = !show"
+                  >
+                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                  </v-btn>
+                </v-card-actions>
+
+                <v-expand-transition>
+                  <div v-show="show">
+                    <v-divider></v-divider>
+
+                    <v-card-text>
+                      I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have
+                      time
+                      for
+                      sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file!
+                      Hey, you
+                      add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way
+                      to
+                      escape.
+                    </v-card-text>
+                  </div>
+                </v-expand-transition>
+              </v-card>
+            </v-hover>
+
+          </v-col>
           <v-col cols="4">
             <v-card
                 class="mx-auto"
@@ -81,7 +174,6 @@
               </v-expand-transition>
             </v-card>
           </v-col>
-
         </v-row>
       </v-container>
     </v-main>
@@ -110,6 +202,16 @@ export default {
   margin: 0 auto;
   text-align: center;
   width: 66%;
+}
+
+.v-card {
+  box-shadow: none;
+}
+
+.library-headline {
+  width: auto;
+  border-top: 4px solid #4285f4;
+  /*margin: 0 32px;*/
 }
 
 </style>
