85adb1789b670746f77bc39ffb41da7e0a8bbeb5
TZZ <1789803837@qq.com>
2020-12-21 01:36:50 +0800

    🖥更新路由样式，增加格式化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,187 +1,219 @@
 <template>
-  <v-app id="home">
-    <v-app-bar app elevate-on-scroll>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-      <v-toolbar-title style="margin-left: 5%">
-        <h2>bing2</h2>
-      </v-toolbar-title>
+    <v-app id="home">
+        <v-app-bar app elevate-on-scroll>
+            <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+            <v-toolbar-title style="margin-left: 5%">
+                <router-link to="/">
+                    <h2>bing2</h2>
+                </router-link>
+            </v-toolbar-title>
 
-      <v-spacer></v-spacer>
-      <v-btn depressed> about </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main class="main">
-      <v-container class="">
-        <div class="library-headline"></div>
-        <v-row>
-          <v-col cols="4">
-            <v-hover v-slot:default="{ hover }">
-              <v-card
-                class="mx-auto"
-                width=""
-                max-width="344"
-                :elevation="hover ? 4 : 0"
-              >
-                <v-img
-                  src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                  height="200px"
-                ></v-img>
+            <v-spacer></v-spacer>
+            <v-btn depressed style="background-color: write"> about </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-main class="main">
+            <v-container class="">
+                <div class="library-headline"></div>
+                <v-row>
+                    <v-col cols="4">
+                        <v-hover v-slot:default="{ hover }">
+                            <v-card
+                                class="mx-auto"
+                                width=""
+                                max-width="344"
+                                :elevation="hover ? 4 : 0"
+                            >
+                                <v-img
+                                    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                                    height="200px"
+                                ></v-img>
 
-                <v-card-title> BLOG </v-card-title>
+                                <v-card-title> BLOG </v-card-title>
 
-                <v-card-subtitle> my personal blog </v-card-subtitle>
-              </v-card>
-            </v-hover>
-          </v-col>
-          <v-col cols="4">
-            <v-hover v-slot:default="{ hover }">
-              <v-card
-                class="mx-auto"
-                width=""
-                max-width="344"
-                :elevation="hover ? 4 : 0"
-              >
-                <v-img src="/img/sunshine.jpg" height="200px"></v-img>
+                                <v-card-subtitle>
+                                    my personal blog
+                                </v-card-subtitle>
+                            </v-card>
+                        </v-hover>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-hover v-slot:default="{ hover }">
+                            <v-card
+                                class="mx-auto"
+                                width=""
+                                max-width="344"
+                                :elevation="hover ? 4 : 0"
+                            >
+                                <v-img
+                                    src="/img/sunshine.jpg"
+                                    height="200px"
+                                ></v-img>
 
-                <v-card-title> Top western road trips </v-card-title>
+                                <v-card-title>
+                                    Top western road trips
+                                </v-card-title>
 
-                <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+                                <v-card-subtitle>
+                                    1,000 miles of wonder
+                                </v-card-subtitle>
 
-                <v-card-actions>
-                  <v-btn color="orange lighten-2" text> Explore </v-btn>
+                                <v-card-actions>
+                                    <v-btn color="orange lighten-2" text>
+                                        Explore
+                                    </v-btn>
 
-                  <v-spacer></v-spacer>
+                                    <v-spacer></v-spacer>
 
-                  <v-btn icon @click="show = !show">
-                    <v-icon>{{
-                      show ? "mdi-chevron-up" : "mdi-chevron-down"
-                    }}</v-icon>
-                  </v-btn>
-                </v-card-actions>
+                                    <v-btn icon @click="show = !show">
+                                        <v-icon>{{
+                                            show
+                                                ? "mdi-chevron-up"
+                                                : "mdi-chevron-down"
+                                        }}</v-icon>
+                                    </v-btn>
+                                </v-card-actions>
 
-                <v-expand-transition>
-                  <div v-show="show">
-                    <v-divider></v-divider>
+                                <v-expand-transition>
+                                    <div v-show="show">
+                                        <v-divider></v-divider>
 
-                    <v-card-text>
-                      I'm a thing. But, like most politicians, he promised more
-                      than he could deliver. You won't have time for sleeping,
-                      soldier, not with all the bed making you'll be doing. Then
-                      we'll go with that data file! Hey, you add a one and two
-                      zeros to that or we walk! You're going to do his laundry?
-                      I've got to find a way to escape.
-                    </v-card-text>
-                  </div>
-                </v-expand-transition>
-              </v-card>
-            </v-hover>
-          </v-col>
-          <v-col cols="4">
-            <v-card class="mx-auto" width="" max-width="344">
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                height="200px"
-              ></v-img>
+                                        <v-card-text>
+                                            I'm a thing. But, like most
+                                            politicians, he promised more than
+                                            he could deliver. You won't have
+                                            time for sleeping, soldier, not with
+                                            all the bed making you'll be doing.
+                                            Then we'll go with that data file!
+                                            Hey, you add a one and two zeros to
+                                            that or we walk! You're going to do
+                                            his laundry? I've got to find a way
+                                            to escape.
+                                        </v-card-text>
+                                    </div>
+                                </v-expand-transition>
+                            </v-card>
+                        </v-hover>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-card class="mx-auto" width="" max-width="344">
+                            <v-img
+                                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                                height="200px"
+                            ></v-img>
 
-              <v-card-title> Top western road trips </v-card-title>
+                            <v-card-title>
+                                Top western road trips
+                            </v-card-title>
 
-              <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+                            <v-card-subtitle>
+                                1,000 miles of wonder
+                            </v-card-subtitle>
 
-              <v-card-actions>
-                <v-btn color="orange lighten-2" text> Explore </v-btn>
+                            <v-card-actions>
+                                <v-btn color="orange lighten-2" text>
+                                    Explore
+                                </v-btn>
 
-                <v-spacer></v-spacer>
+                                <v-spacer></v-spacer>
 
-                <v-btn icon @click="show = !show">
-                  <v-icon>{{
-                    show ? "mdi-chevron-up" : "mdi-chevron-down"
-                  }}</v-icon>
-                </v-btn>
-              </v-card-actions>
+                                <v-btn icon @click="show = !show">
+                                    <v-icon>{{
+                                        show
+                                            ? "mdi-chevron-up"
+                                            : "mdi-chevron-down"
+                                    }}</v-icon>
+                                </v-btn>
+                            </v-card-actions>
 
-              <v-expand-transition>
-                <div v-show="show">
-                  <v-divider></v-divider>
+                            <v-expand-transition>
+                                <div v-show="show">
+                                    <v-divider></v-divider>
 
-                  <v-card-text>
-                    I'm a thing. But, like most politicians, he promised more
-                    than he could deliver. You won't have time for sleeping,
-                    soldier, not with all the bed making you'll be doing. Then
-                    we'll go with that data file! Hey, you add a one and two
-                    zeros to that or we walk! You're going to do his laundry?
-                    I've got to find a way to escape.
-                  </v-card-text>
+                                    <v-card-text>
+                                        I'm a thing. But, like most politicians,
+                                        he promised more than he could deliver.
+                                        You won't have time for sleeping,
+                                        soldier, not with all the bed making
+                                        you'll be doing. Then we'll go with that
+                                        data file! Hey, you add a one and two
+                                        zeros to that or we walk! You're going
+                                        to do his laundry? I've got to find a
+                                        way to escape.
+                                    </v-card-text>
+                                </div>
+                            </v-expand-transition>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <div class="library-headline"></div>
+                <v-row>
+                    <v-col v-for="n in 3" :key="n" cols="4">
+                        <v-hover v-slot:default="{ hover }">
+                            <v-card height="200" :elevation="hover ? 4 : 0"
+                                >IM FUN</v-card
+                            >
+                        </v-hover>
+                    </v-col>
+                </v-row>
+                <v-divider></v-divider>
+                <div class="library-headline"></div>
+                <v-row>
+                    <v-col cols="4">
+                        <TCard :image="image" />
+                    </v-col>
+                    <v-col cols="4">
+                        <TCard :image="image" />
+                    </v-col>
+                    <v-col cols="4">
+                        <TCard :image="image" />
+                    </v-col>
+                </v-row>
+                <div class="library-headline"></div>
+                <div class="text-center">
+                    <v-pagination v-model="page" :length="6"></v-pagination>
                 </div>
-              </v-expand-transition>
-            </v-card>
-          </v-col>
-        </v-row>
-        <div class="library-headline"></div>
-        <v-row>
-          <v-col v-for="n in 3" :key="n" cols="4">
-            <v-hover v-slot:default="{ hover }">
-              <v-card height="200" :elevation="hover ? 4 : 0">IM FUN</v-card>
-            </v-hover>
-          </v-col>
-        </v-row>
-        <v-divider></v-divider>
-        <div class="library-headline"></div>
-        <v-row>
-          <v-col cols="4">
-            <TCard :image="image" />
-          </v-col>
-          <v-col cols="4">
-            <TCard :image="image" />
-          </v-col>
-          <v-col cols="4">
-            <TCard :image="image" />
-          </v-col>
-        </v-row>
-        <div class="library-headline"></div>
-        <div class="text-center">
-          <v-pagination v-model="page" :length="6"></v-pagination>
-        </div>
-      </v-container>
-    </v-main>
-  </v-app>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 import TCard from "@/components/TCard";
 // @ is an alias to /src
 export default {
-  name: "Home",
-  components: {
-    TCard,
-  },
-  data: function () {
-    return {
-      show: false,
-      image: {
-        title: "BLOG",
-        subtitle: "this is my personl blog.",
-        url: "/images/sunshine.jpg",
-      },
-      page: 1,
-    };
-  },
+    name: "Home",
+    components: {
+        TCard
+    },
+    data: function() {
+        return {
+            show: false,
+            image: {
+                title: "BLOG",
+                subtitle: "this is my personl blog.",
+                url: "/images/sunshine.jpg"
+            },
+            page: 1
+        };
+    }
 };
 </script>
 
 <style scoped>
 .home {
-  text-align: center;
+    text-align: center;
 }
 
 .main {
-  margin: 0 auto;
-  text-align: center;
-  /* width: 66%; */
+    margin: 0 auto;
+    text-align: center;
+    /* width: 66%; */
 }
 
 .v-card {
-  box-shadow: none;
+    box-shadow: none;
 }
 </style>
