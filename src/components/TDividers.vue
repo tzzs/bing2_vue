a4dfbad6a6f3fdb079c575f4db7b62cc27d3c770
TZZ <1789803837@qq.com>
2020-12-24 01:15:35 +0800

    🔩使用TDividers组件代替div分割线，可自定义颜色

---
+++ src/components/TDividers.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="tdividers" :style="{ borderTop: '4px solid ' + color }"></div>
+</template>
+
+<script>
+export default {
+    name: "TDividers",
+    props: {
+        color: String
+    }
+};
+</script>
+
+<style scoped>
+/* 分隔符 */
+#tdividers {
+    width: auto;
+    /* border-top: 4px solid #4285f4; */
+    /*margin: 0 32px;*/
+}
+</style>
