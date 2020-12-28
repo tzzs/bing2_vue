e951f288279a569ecc77d4e3d95bcb7e52f82a87
TZZ <1789803837@qq.com>
2020-12-29 00:18:51 +0800

    🔩拆分页脚组件

---
+++ src/components/TFooter.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <!-- 页脚 -->
+        <v-divider></v-divider>
+        <v-footer padless color="white">
+            <v-container>
+                <v-row
+                    justify="space-between"
+                    style="margin-left: 1%;margin-right: 1%"
+                >
+                    <v-col cols="3" align="left">
+                        <div>
+                            <h3>BING2</h3>
+                            <br />
+                            <p>
+                                本站所有图片均来自
+                                <a href="https://cn.bing.com/" target="_blank">
+                                    必应搜索。
+                                </a>
+                                <br />
+                                All pictures on this site are from
+                                <a href="https://cn.bing.com/" target="_blank">
+                                    Bing search.
+                                </a>
+                            </p>
+                        </div>
+                    </v-col>
+                    <v-col cols="4" align="left">
+                        <v-row>
+                            <v-col cols="4">
+                                <div style="margin-left: 10%">
+                                    <h3>Discover</h3>
+                                    <br />
+                                    <ul>
+                                        <li>Blog</li>
+                                        <li>GitHub</li>
+                                    </ul>
+                                </div>
+                            </v-col>
+                            <v-col cols="4">
+                                <div style="margin-left: 10%">
+                                    <h3>Connect</h3>
+                                    <br />
+                                    <ul>
+                                        <li>
+                                            Github Issue
+                                        </li>
+                                        <li>
+                                            About
+                                        </li>
+                                    </ul>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+                <v-row style="margin-left: 1%;margin-right: 1%">
+                    <v-col cols="3" align="left">
+                        <router-link to="https://imtzz.com">
+                            <p>
+                                <a href="https://imtzz.com"
+                                    >Copyright ©2020 IMTZZ.
+                                </a>
+                            </p>
+                        </router-link>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TFooter"
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,85 +64,21 @@
                 </div>
             </v-container>
 
-            <!-- 页脚 -->
-            <v-divider></v-divider>
-            <v-footer padless color="white">
-                <v-container>
-                    <v-row
-                        justify="space-between"
-                        style="margin-left: 1%;margin-right: 1%"
-                    >
-                        <v-col cols="3" align="left">
-                            <div>
-                                <h3>BING2</h3>
-                                <br />
-                                <p>
-                                    本站所有图片均来自
-                                    <a href="https://cn.bing.com/" target="_blank">
-                                        必应搜索。
-                                    </a>
-                                    <br />
-                                    All pictures on this site are from
-                                    <a href="https://cn.bing.com/" target="_blank">
-                                        Bing search.
-                                    </a>
-                                </p>
-                            </div>
-                        </v-col>
-                        <v-col cols="4" align="left">
-                            <v-row>
-                                <v-col cols="4">
-                                    <div style="margin-left: 10%">
-                                        <h3>Discover</h3>
-                                        <br />
-                                        <ul>
-                                            <li>Blog</li>
-                                            <li>GitHub</li>
-                                        </ul>
-                                    </div>
-                                </v-col>
-                                <v-col cols="4">
-                                    <div style="margin-left: 10%">
-                                        <h3>Connect</h3>
-                                        <br />
-                                        <ul>
-                                            <li>
-                                                Github Issue
-                                            </li>
-                                            <li>
-                                                About
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </v-col>
-                            </v-row>
-                        </v-col>
-                    </v-row>
-                    <v-row style="margin-left: 1%;margin-right: 1%">
-                        <v-col cols="3" align="left">
-                            <router-link to="https://imtzz.com">
-                                <p>
-                                    <a href="https://imtzz.com"
-                                        >Copyright ©2020 IMTZZ.
-                                    </a>
-                                </p>
-                            </router-link>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-footer>
+            <TFooter />
         </v-main>
     </v-app>
 </template>
 
 <script>
 import TCalendarImages from "@/components/TCalendarImages";
+import TFooter from "@/components/TFooter";
 
 // @ is an alias to /src
 export default {
     name: "Home",
     components: {
-        TCalendarImages
+        TCalendarImages,
+        TFooter
     },
     data: function() {
         return {
